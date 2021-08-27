bd4962890eaa56b1bcfbb3930e46070095dd50b3
fangxing <fangxing1996@126.com>
2021-08-27 23:36:59 +0800

    change the space before ;

--- ftok/main.cpp
+++ ftok/main.cpp
@@ -8,24 +8,24 @@
 using namespace std;
 int main(int argc, char** argv) {
 
-    struct stat stat1 ;
+    struct stat stat1;
     if ( argc != 2 ){
 
-        printf("usage: ftok < pathname >" ) ;
-        exit(1) ;
+        printf("usage: ftok < pathname >" );
+        exit(1);
     }
-    stat(argv[1], &stat1) ;
+    stat(argv[1], &stat1);
     printf("st_dev:%lx, st_ino:%lx, key:%x\n",  \
-        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x587 )) ;
+        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x587 ));
     printf("st_dev:%lx, st_ino:%lx, key:%x\n",  \
-        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x118 )) ;
+        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x118 ));
     printf("st_dev:%lx, st_ino:%lx, key:%x\n",  \
-        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x22)) ;
+        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x22));
     printf("st_dev:%lx, st_ino:%lx, key:%x\n",  \
-        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x33)) ;
+        (unsigned long)stat1.st_dev, (unsigned long)stat1.st_ino , ftok(argv[1], 0x33));
     /*
         由输出结果可以观察到，函数ftok返回的32bit的key值由3个部分先后组成：proj_id的低8bits，
         st_dev的低8bits以及st_ino的低位16bits。
     */
-    exit(0) ;
+    exit(0);
 }
